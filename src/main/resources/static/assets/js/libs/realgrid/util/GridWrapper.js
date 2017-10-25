7824d592a2d9fcc0d293da3a92cabd794c1f20f6
hyukjun-lee <cool2grey@gmail.com>
2017-10-25 17:00:10 +0900

    1. GridWrapper 기능 추가    - 엑셀데이터 붙여넣기 기능 추가

--- src/main/resources/static/assets/js/libs/realgrid/util/GridWrapper.js
+++ src/main/resources/static/assets/js/libs/realgrid/util/GridWrapper.js
@@ -137,6 +137,23 @@ var GridWrapper = function(p_id,p_rootContext,_isTree) {
             innerDraggable : true,		//Inner Drag & Drop기능 사용 여부를 지정한다
         	enterToTab : false
 		},
+		paste : {
+            singleMode: false,	//포커스를 갖는 셀 하나에만 붙여넣기
+            enabled: true,	//Paste가능여부 설정
+            startEdit: true,	//붙여넣게 될 값이 복수 행이 아니고, 붙여넣을 행이 아직 편집 중이 아니면 편집을 시작
+            commitEdit: true,	//복수 행 붙여넣기일 때 기존 편집 상태를 commit
+            enableAppend: true,	//붙여넣을 복수 행이 기존 행의 범위를 넘어설 때 행을 추가
+            fillFieldDefaults: true,	//행 추가시 포함되지 않은 필드의 값을 data field의 기본값으로 채움
+            fillColumnDefaults: true,	//행 추가시 포함되지 않은 필드의 값을 컬럼의 기본값으로 채움
+            forceRowValidation: true,	//복수행 붙여 넣기 중 행별로 행 validation을 실행
+            forceColumnValidation: true,	//복수행 붙여 넣기 중 행별로 컬럼 validation을 실행
+            selectionBase: false,	// true면 focus 셀이 포함된 선택 영역의 처음 셀부터 붙여넣기 시작
+            stopOnError: false,	//Validation이 실패하거나 형변환이 실패하면 나머지 붙여넣기를 중지하고 예외를 발생
+            noEditEvent: false,	//단일행을 붙여넣기 할때 Cell마다 발생하는 onEditRowChanged가 발생하지 않는다.
+            eventEachRow: true,	//복수행을 붙여넣기 할때 각 행마다 onEditRowPasted이벤트가 발생
+            checkReadOnly: true,	//readOnly이거나 editable이 false인 Column은 paste대상에서 제외
+            checkDomainOnly: true	// DropDown Editor의 domainOnly가 true인 컬럼에 붙여넣기 할때 values에 없는 값은 붙여넣기 되지 않음
+		},
 		sort : {
 			enabled : true
 		},
@@ -554,6 +571,8 @@ var GridWrapper = function(p_id,p_rootContext,_isTree) {
             gridView.setStyles(defaultStyle.parsing);
             gridView.setFilteringOptions(filteringDefaultOptions);
             gridView.setDisplayOptions(displayDefaultOption);
+            gridView.setPasteOptions(_gridDefaultOption.paste);
+
             dataProvider.setOptions(providerDefaultOption);
             // gridView.setStyles(defaultStyle.event.selection);
 		}
