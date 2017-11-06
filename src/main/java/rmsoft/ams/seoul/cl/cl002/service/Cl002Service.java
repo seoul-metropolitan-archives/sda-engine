a6891209c792dd13467407e303333e784a356a78
js.park <js.park@dream-ant.com>
2017-11-06 15:19:07 +0900

    [변경내용] 1. Class Uuid 및 Order Key 생성

--- src/main/java/rmsoft/ams/seoul/cl/cl002/service/Cl002Service.java
+++ src/main/java/rmsoft/ams/seoul/cl/cl002/service/Cl002Service.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 import rmsoft.ams.seoul.cl.cl001.vo.Cl00101VO;
 import rmsoft.ams.seoul.cl.cl002.dao.Cl002Mapper;
@@ -20,8 +21,6 @@ import rmsoft.ams.seoul.cl.cl002.vo.Cl00201VO;
 import rmsoft.ams.seoul.cl.cl002.vo.Cl00202VO;
 import rmsoft.ams.seoul.common.domain.ClClass;
 import rmsoft.ams.seoul.common.domain.ClClassCon;
-import rmsoft.ams.seoul.common.domain.ClClassificationScheme;
-import rmsoft.ams.seoul.common.domain.ClClassificationSchemeCon;
 import rmsoft.ams.seoul.common.repository.ClClassRepository;
 import rmsoft.ams.seoul.utils.CommonCodeUtils;
 
@@ -37,9 +36,12 @@ public class Cl002Service extends BaseService {
     private Cl002Mapper cl002Mapper;
 
     @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
     private ClClassRepository clClassRepository;
 
-    public Cl00101VO getClassificationScheme(){
+    public Cl00101VO getClassificationScheme() {
 
         return cl002Mapper.getClassificationScheme();
     }
@@ -61,7 +63,7 @@ public class Cl002Service extends BaseService {
 
         Cl00201VO cl00201VO = new Cl00201VO();
         cl00201VO.setClassificationSchemeUuid(requestParams.getString("classificationSchemeUuid"));
-        if(StringUtils.isNotEmpty(requestParams.getString("orderNo")) ){
+        if (StringUtils.isNotEmpty(requestParams.getString("orderNo"))) {
             cl00201VO.setOrderNo(Integer.parseInt(requestParams.getString("orderNo")));
         }
         cl00201VO.setOrderKey(requestParams.getString("orderKey"));
@@ -90,17 +92,17 @@ public class Cl002Service extends BaseService {
         return cl002Mapper.getSelectedClassDetail(cl00201VO);
     }
 
-    public void updateStatusConfirm(List<Cl00201VO> lists){
-        List<ClClass> clClassList = ModelMapperUtils.mapList(lists,ClClass.class);
+    public void updateStatusConfirm(List<Cl00201VO> lists) {
+        List<ClClass> clClassList = ModelMapperUtils.mapList(lists, ClClass.class);
         ClClass orgClClass = null;
         String changeStatusNm = "";
         String changeStatusCd = "";
         int index = 0;
         for (Cl00201VO cl00201VO : lists) {
             orgClClass = clClassRepository.findOne(clClassList.get(index).getId());
-            changeStatusNm = cl00201VO.getChangeStatus() == "" ?  "Draft" : cl00201VO.getChangeStatus();
-            changeStatusCd = CommonCodeUtils.getCodeDetailUuid("CD113",changeStatusNm);
-            if(orgClClass.getStatusUuid() != changeStatusCd){
+            changeStatusNm = cl00201VO.getChangeStatus() == "" ? "Draft" : cl00201VO.getChangeStatus();
+            changeStatusCd = CommonCodeUtils.getCodeDetailUuid("CD113", changeStatusNm);
+            if (orgClClass.getStatusUuid() != changeStatusCd) {
                 cl00201VO.setStatusUuid(changeStatusCd);
                 cl00201VO.setUpdateUuid(SessionUtils.getCurrentLoginUserUuid());
                 cl00201VO.setUpdateDate(Timestamp.valueOf(DateUtils.convertToString(LocalDateTime.now(), DateUtils.DATE_TIME_PATTERN)));
@@ -110,15 +112,15 @@ public class Cl002Service extends BaseService {
         }
     }
 
-    public ApiResponse updateStatusCancel(List<Cl00201VO> list){
-        List<ClClass> clClassList = ModelMapperUtils.mapList(list,ClClass.class);
+    public ApiResponse updateStatusCancel(List<Cl00201VO> list) {
+        List<ClClass> clClassList = ModelMapperUtils.mapList(list, ClClass.class);
         ClClass orgClClass = null;
         int index = 0;
         String changeStatus = "";
         for (ClClass clClass : clClassList) {
-            changeStatus = list.get(index).getChangeStatus() == "" ?  "Draft" : list.get(index).getChangeStatus();
+            changeStatus = list.get(index).getChangeStatus() == "" ? "Draft" : list.get(index).getChangeStatus();
             orgClClass = clClassRepository.findOne(clClass.getId());
-            clClass.setStatusUuid(CommonCodeUtils.getCodeDetailUuid("CD113",changeStatus));
+            clClass.setStatusUuid(CommonCodeUtils.getCodeDetailUuid("CD113", changeStatus));
             //clClass.setOrderKey(orgClClass.getOrderKey());
             clClass.setInsertDate(orgClClass.getInsertDate());
             clClass.setInsertUuid(orgClClass.getInsertUuid());
@@ -128,28 +130,29 @@ public class Cl002Service extends BaseService {
         return ApiResponse.of(ApiStatus.SUCCESS, "SUCCESS");
     }
 
-    public ApiResponse updateClassList(List<Cl00201VO> list){
-        List<ClClass> clClassList = ModelMapperUtils.mapList(list,ClClass.class);
+    public ApiResponse updateClassList(List<Cl00201VO> list) {
+        List<ClClass> clClassList = ModelMapperUtils.mapList(list, ClClass.class);
         ClClass orgClClass = null;
         ClClassCon clClassCon = null;
         String ctUuid = "";
         String detailCode = "";
         String maxCode = "";
         String maxDefaultCode = "";
+
         for (ClClass clClass : clClassList) {
-            if(StringUtil.isNullOrEmpty(clClass.getClassificationSchemeUuid())){ //Insert
+            if (StringUtil.isNullOrEmpty(clClass.getClassUuid())) { //Insert
 
                 maxCode = getMaxClassCode(clClass.getClassificationSchemeUuid());
-                if(StringUtils.isNotEmpty(maxCode)){ //분류코드 조합
-                    maxDefaultCode = StringUtils.trim(maxCode).substring(0,7);
+                if (StringUtils.isNotEmpty(maxCode)) { //분류코드 조합
+                    maxDefaultCode = StringUtils.trim(maxCode).substring(0, 7);
                     ctUuid = StringUtils.trim(maxCode).substring(7);
                     ctUuid = String.valueOf(Integer.parseInt(ctUuid) + 1);
 
-                    if(ctUuid.length() == 1 ){
+                    if (ctUuid.length() == 1) {
                         ctUuid = "000" + ctUuid;
-                    }else if(ctUuid.length() == 2 ){
+                    } else if (ctUuid.length() == 2) {
                         ctUuid = "00" + ctUuid;
-                    }else if(ctUuid.length() == 3){
+                    } else if (ctUuid.length() == 3) {
                         ctUuid = "0" + ctUuid;
                     }
                     detailCode = maxDefaultCode + ctUuid;
@@ -157,19 +160,23 @@ public class Cl002Service extends BaseService {
 
                 clClass.setClassUuid(UUIDUtils.getUUID()); //UUID 생성
                 clClass.setClassCode(detailCode);
+
+                // Oracle Function Call
+                String orderKey = jdbcTemplate.queryForObject("select AMS.FC_CL_CLASS_SORTKEY('" + clClass.getParentClassUuid() + "' , '" + clClass.getOrderNo() + "') from dual", String.class);
+                clClass.setOrderKey(orderKey);
             }
 
             if (clClass.isCreated() || clClass.isModified()) {
-                clClass.setStatusUuid(CommonCodeUtils.getCodeDetailUuid("CD113","Draft"));
+                clClass.setStatusUuid(CommonCodeUtils.getCodeDetailUuid("CD113", "Draft"));
 
-                if(clClass.isModified()) {
+                if (clClass.isModified()) {
                     orgClClass = clClassRepository.findOne(clClass.getId());
                     clClass.setInsertDate(orgClClass.getInsertDate());
                     clClass.setInsertUuid(orgClClass.getInsertUuid());
                 }
                 clClassRepository.save(clClass);
             } else if (clClass.isDeleted()) {
-                if(getChildClass(clClass.getClassUuid()) == 0 ){
+                if (getChildClass(clClass.getClassUuid()) == 0) {
                     clClassRepository.delete(clClass);
                 }
             }
@@ -178,8 +185,11 @@ public class Cl002Service extends BaseService {
         return ApiResponse.of(ApiStatus.SUCCESS, "SUCCESS");
     }
 
-    public int getChildClass(String classUuid){
-        return  cl002Mapper.getChildClass(classUuid);
+    public int getChildClass(String classUuid) {
+        return cl002Mapper.getChildClass(classUuid);
     }
-    public String getMaxClassCode(String classificationSchemeUuid){return  cl002Mapper.getMaxClassCode(classificationSchemeUuid);}
+
+    public String getMaxClassCode(String classificationSchemeUuid) {
+        return cl002Mapper.getMaxClassCode(classificationSchemeUuid);
+    }
 }
