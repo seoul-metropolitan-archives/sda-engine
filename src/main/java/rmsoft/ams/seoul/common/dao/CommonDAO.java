fcdbc87fd3b91d6c7d66ac663cecf855a2b1361a
js.park <js.park@dream-ant.com>
2017-12-11 15:50:26 +0900

    [변경내용] 1. 전자정부 Framework적용 2. pdf 뷰어 cross-domain 관련 플러그인 추가

---
+++ src/main/java/rmsoft/ams/seoul/common/dao/CommonDAO.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2017. RMSoft Co.,Ltd. All rights reserved
+ */
+
+package rmsoft.ams.seoul.common.dao;
+
+import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The type Common Dao.
+ */
+public class CommonDAO extends EgovAbstractDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * Search list.
+     *
+     * @param sql   the sql
+     * @param param the param
+     * @return the list
+     */
+    public List<Map<String, Object>> search(String sql, Map<String, Object> param) {
+        //param.get("searchField");\$([A-Z])*\$
+
+        sql = sql.replaceAll("@([A-Za-z1-9_])*", null == param.get("searchField") ? "''" : "'" + param.get("searchField").toString() + "'");
+        sql = sql.replaceAll(";", "");
+        System.out.println("sql=>" + sql);
+        return jdbcTemplate.queryForList(sql);
+    }
+
+}
