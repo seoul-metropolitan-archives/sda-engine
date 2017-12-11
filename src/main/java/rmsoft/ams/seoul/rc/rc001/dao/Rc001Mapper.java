f5ea144a3ebcd20c8d69fc67a28a4cb644011bd7
hyukjunlee <cool2grey@gmail.com>
2017-12-11 10:17:45 +0900

    1. Record Explorer 개발 완료 90%

--- src/main/java/rmsoft/ams/seoul/rc/rc001/dao/Rc001Mapper.java
+++ src/main/java/rmsoft/ams/seoul/rc/rc001/dao/Rc001Mapper.java
@@ -3,10 +3,7 @@ package rmsoft.ams.seoul.rc.rc001.dao;
 import io.onsemiro.core.mybatis.MyBatisMapper;
 import org.apache.ibatis.annotations.Mapper;
 import rmsoft.ams.seoul.common.domain.RcAggregation;
-import rmsoft.ams.seoul.rc.rc001.vo.Rc00101VO;
-import rmsoft.ams.seoul.rc.rc001.vo.Rc00102VO;
-import rmsoft.ams.seoul.rc.rc001.vo.Rc00103VO;
-import rmsoft.ams.seoul.rc.rc001.vo.Rc00104VO;
+import rmsoft.ams.seoul.rc.rc001.vo.*;
 
 import java.util.List;
 import java.util.Map;
@@ -37,6 +34,6 @@ public interface Rc001Mapper extends MyBatisMapper {
 
     long getChildrenCnt (Rc00101VO param);
     int checkPublished(Rc00101VO param);
-
+    int move(Rc00106VO data);
     int updateState(RcAggregation data);
 }
---
+++ src/main/java/rmsoft/ams/seoul/rc/rc001/vo/Rc00106VO.java
@@ -0,0 +1,8 @@
+package rmsoft.ams.seoul.rc.rc001.vo;
+
+import lombok.Data;
+
+@Data
+public class Rc00106VO extends Rc00101VO{
+    private String updateUuid;
+}
