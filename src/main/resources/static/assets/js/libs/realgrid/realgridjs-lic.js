828d71a6c1d5d48beb86ef9c338f65f14d452220
younghyun shin <yh.shin@dream-ant.com>
2018-06-01 15:43:11 +0900

    라이센스변경

--- src/main/resources/static/assets/js/libs/realgrid/realgridjs-lic.js
+++ src/main/resources/static/assets/js/libs/realgrid/realgridjs-lic.js
@@ -1,1 +1,1 @@
-var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UvKqt1f0nYq0Ni2LhgMoQgo0dG9IxGODqqY3nEiKYzppQ==';
+var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgvr3+iyFPC2UsN1/2t5p0Lv9oa/vm+R0YadDrVfKmXZ8VWC+J11rOuc40oHsPcuPqH+6ZGVxDxR7Q=';
