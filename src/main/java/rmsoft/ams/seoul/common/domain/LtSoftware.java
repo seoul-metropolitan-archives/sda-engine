b1b91f887b412050dfc95802f68faa93524f28a4
yoyodjk <dj.kim@dream-ant.com>
2018-06-19 10:12:35 +0900

    [반영내용] 1. 장기보존 Software/Tool/Pathway 1차 개발 완료

---
+++ src/main/java/rmsoft/ams/seoul/common/domain/LtSoftware.java
@@ -0,0 +1,68 @@
+package rmsoft.ams.seoul.common.domain;
+
+import io.onsemiro.core.domain.SimpleJpaModel;
+import lombok.*;
+import org.apache.ibatis.type.Alias;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Setter
+@Getter
+@DynamicInsert
+@DynamicUpdate
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "LT_SOFTWARE")
+@IdClass(LtSoftware.LtSoftwareId.class)
+@Alias("LtSoftware")
+public class LtSoftware extends SimpleJpaModel<LtSoftware.LtSoftwareId> {
+
+	@Id
+	@Column(name = "SOFTWARE_UUID", length = 36, nullable = false)
+	private String softwareUuid;
+
+	@Column(name = "SOFTWARE_NAME", length = 500, nullable = false)
+	private String softwareName;
+
+	@Column(name = "SOFTWARE_VERSION", length = 10)
+	private String softwareVersion;
+
+	@Column(name = "VENDOR_NAME", length = 100)
+	private String vendorName;
+
+	@Column(name = "VENDOR_HOMEPAGE", length = 100)
+	private String vendorHomepage;
+
+	@Column(name = "LICENSE_CODE", length = 100)
+	private String licenseCode;
+
+	@Column(name = "INSTALL_PATH", length = 100, nullable = false)
+	private String installPath;
+
+	@Column(name = "EXE_FILE_NAME", length = 50, nullable = false)
+	private String exeFileName;
+
+	@Column(name = "USE_YN", length = 1, nullable = false)
+	private String useYn;
+
+
+
+	@Override
+	public LtSoftware.LtSoftwareId getId() { return LtSoftware.LtSoftwareId.of(softwareUuid); }
+
+	/**
+	 * The type Ad entity type id.
+	 */
+	@Embeddable
+	@Data
+	@NoArgsConstructor
+	@RequiredArgsConstructor(staticName = "of")
+	public static class LtSoftwareId implements Serializable {
+
+		@NonNull
+		private String softwareUuid;
+	}
+}
