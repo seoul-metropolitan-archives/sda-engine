791be947525d14955b202c9b1193fc348f60bd93
younghyun shin <yh.shin@dream-ant.com>
2018-11-07 10:42:12 +0900

    test page

--- base/src/main/java/rmsoft/ams/seoul/at/at001/dao/At001Mapper.java
+++ base/src/main/java/rmsoft/ams/seoul/at/at001/dao/At001Mapper.java
@@ -4,6 +4,7 @@ import io.onsemiro.core.mybatis.MyBatisMapper;
 import rmsoft.ams.seoul.at.at001.vo.At00101VO;
 import rmsoft.ams.seoul.at.at001.vo.At00102VO;
 import rmsoft.ams.seoul.at.at001.vo.At00103VO;
+import rmsoft.ams.seoul.at.at002.vo.At00201VO;
 
 import java.util.List;
 
@@ -14,4 +15,5 @@ public interface At001Mapper extends MyBatisMapper {
     List<At00101VO> getAuthorityList(At00101VO at00101VO);
     List<At00102VO> getRelAuthorityList(At00102VO at00102VO);
     List<At00103VO> getAuthorityMetaInfoList(At00103VO at00103VO);
+    At00102VO getAuthorityRelation(At00102VO at00102VO);
 }
