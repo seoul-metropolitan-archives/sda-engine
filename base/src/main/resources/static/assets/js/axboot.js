1a92cc6e07bed4e4e41a2c67a4a8fcffcdf30384
gamjaradio@gmail.com <gamja119>
2018-12-21 22:54:10 +0900

    merge

--- base/src/main/resources/static/assets/js/axboot.js
+++ base/src/main/resources/static/assets/js/axboot.js
@@ -3235,10 +3235,11 @@ axboot.viewExtend = function (_obj1, _obj2) {
     try {
         if(!_obj2.noPermission){
             if (!isPermission(_obj2.entityName, FUNCTION_ADD_UUID)) {
-                $("#"+_obj2.tagId).parents().eq(1).find(".btn_a").attr('disabled', true);
+                //일단 주석
+                /*$("#"+_obj2.tagId).parents().eq(1).find(".btn_a").attr('disabled', true);*/
             }
             if (!isPermission(_obj2.entityName, FUNCTION_DEL_UUID)) {
-                $("#"+_obj2.tagId).parents().eq(1).find(".btn_d").attr('disabled', true);
+                //일단 주석/*$("#"+_obj2.tagId).parents().eq(1).find(".btn_d").attr('disabled', true);*/
             }
         }
     }catch(e){
